ef659384baae9c5dedff62d714dc11321f9eaaf4
harikongit <harikrishna.nakka@innovapost.com>
2026-07-08 11:31:48 -0400

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: './', 
   plugins: [react()],
   build: {
     minify: 'esbuild', // Use esbuild for minification (faster and included with Vite)
